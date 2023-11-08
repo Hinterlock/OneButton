3a8e5b4b64551e2c4cc6d00ff401d51e71dc560a
Hinterlock <102774385+Hinterlock@users.noreply.github.com>
2023-11-07 17:55:46 -0800

    improved flavor

--- docs/game/main.js
+++ docs/game/main.js
@@ -1,9 +1,11 @@
-title = "Morse Encoder!";
+title = "Number Cruncher";
 
 description = `
+[Tap] Zero
+[Hold] One
 `;
 
-characters = [];
+characters = []; // for sprites (none atm)
 
 const G = {
 	WIDTH: 100,
@@ -13,10 +15,13 @@ const G = {
 options = {
 	viewSize: {x: G.WIDTH, y: G.HEIGHT},
 	isPlayingBgm: true,
+	theme: "crt"
 };
 
 // constants
-const questionTime = 1000;
+const questionTime = 600;
+const maxTime = 2000;
+const holdTime = 10;
 
 // variables
 let isPressing;		// checking if button is held
@@ -25,16 +30,27 @@ let nextQ;			// check to ask next question
 let timeout;		// keeping track of time till timeout into next question
 let answer; 		// player input
 let correctAnswer;	// what input should match
-let txt;			// display text
+let binaryTxt;		// display text in binary
+let b10Txt;			// display text in base 10
 
 // difficulty = digits
-function Generator(digits){
+function binaryArrayToStr(arr) { // function for printing arrays without commas
+	let s = "";
+	for (let i = 0; i < arr.length; i++) {
+		s = s.concat(String(arr[i]));
+		s = s.concat(" ");
+	}
+	return s;
+}
+function Generator(digits){ // generates a number and saves it in str form to binaryTxt as binary and b10Txt as base 10
 	let resD = 0;
 	for(let x in range(digits)){
 		correctAnswer[parseInt(x)] = floor(Math.random()*2);
 		resD += correctAnswer[parseInt(x)] * Math.pow(2, digits - parseInt(x) - 1);
 	}
-	txt = String(resD);
+	//txt = String(resD);
+	binaryTxt = binaryArrayToStr(correctAnswer);
+	b10Txt = String(resD);
 }
 
 function update() {
@@ -45,29 +61,41 @@ function update() {
 		timeout = 0;
 		answer = [];
 		correctAnswer = [];
-		txt = "testing";
+		binaryTxt = "testing";
 	}
 	// Gen prompt
 	if (nextQ) {
+		/*timeout += questionTime; // if you want question time to carry over
+		if (timeout > maxTime) {
+			timeout = maxTime;
+		}*/
 		timeout = questionTime;
 		// ask question
 		Generator(5);
-		console.log(txt);
+		console.log(b10Txt);
+		console.log(binaryTxt);
 		console.log(correctAnswer);
 		nextQ = false;
 	}
-	
-	box(vec(timeout*100/questionTime, 10), 2); // timeout bar
-	text(String(floor(timeout*10/60)/10), vec(50-5, 2)); // timeout number
 
-	text(txt, vec(G.WIDTH/4, 15)); // question
-	text(String(answer), vec(G.WIDTH/4, 22)); // input so far
+	color("red");
+	line(vec(0, 10), vec(timeout*100/questionTime, 10)); // timeout bar
+	text(String(floor(timeout*10/60)/10), vec(G.WIDTH/2-15, 4)); // timeout number
 
+	color("black");
+	text(b10Txt, vec(5, G.HEIGHT/4)); // question
+	color("light_black");
+	text(":", vec(15, G.HEIGHT/4));
+	color("black");
+	text(binaryTxt, vec(G.WIDTH/4, G.HEIGHT/4));
+	text(binaryArrayToStr(answer), vec(G.WIDTH/4, G.HEIGHT/2 + 10)); // input so far
+
+	color("red");
 	line(vec(0, G.HEIGHT*3/4), vec(G.WIDTH, G.HEIGHT*3/4)); // separator line for cancel area
-	text("Cancel", vec(G.WIDTH/4, G.HEIGHT*7/8)); // cancel text
+	text("Cancel", vec(G.WIDTH/2-16, G.HEIGHT*7/8)); // cancel text
 
-	if(held > 10 && isPressing) { // indicator that youve held long enough
-		box(vec(input.pos), 2);
+	if(held > holdTime && isPressing) { // indicator that youve held long enough
+		box(vec(input.pos), 10);
 	}
 
 	// Input Handling
@@ -76,16 +104,20 @@ function update() {
 		held = 0;
 	}
 	held += 1;
+	/*if (!isPressing && held > holdTime) {
+		// third kind of input: pause between inputs (ie for breaks btwn morse letters)
+		// note that you'll have to make a variable that makes sure it only registers the pause once
+	}*/
 	if (input.isJustReleased) {
 		isPressing = false;
 		if(input.pos.y > G.HEIGHT*3/4) { // cancel input
 			console.log("canceled");
 			answer = [];
 		} else {
-			if (held > 10) { //held
+			if (held > holdTime) { //held
 				console.log("held");
 				console.log(held);
-				play("lucky");
+				play("laser");
 				answer.push(1);
 			} else { //tapped
 				play("select");
@@ -108,14 +140,18 @@ function update() {
 		if (correct) {
 			nextQ = true;
 			console.log("YIPPEEE!");
-			score += 1;
+			play("lucky");
+			score += timeout/100;
+		} else {
+			play("explosion");
 		}
 		// empty answer
 		answer = [];
 	}
 	if (timeout <= 0) { // Check if prompt timed out
-		nextQ = true;
-		answer = [];
+		end();
+		//nextQ = true;
+		//answer = [];
 	} else {
 		timeout -= 1;
 	}
